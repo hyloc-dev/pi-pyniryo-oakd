966f5b7ee3c34c1c16f4daa4c8b6886c8e7cac2d
bortpro <70864791+bortpro@users.noreply.github.com>
2023-12-17 00:20:48 -0800

    Add files via upload

--- record_cam.py
+++ record_cam.py
@@ -1,7 +1,8 @@
+
 import depthai as dai
+import time
 
-def record_oak():
-
+def record_oak(duration_seconds=5):
     # Create pipeline
     pipeline = dai.Pipeline()
 
@@ -23,25 +24,27 @@ def record_oak():
 
     # Connect to device and start pipeline
     with dai.Device(pipeline) as device:
-
         # Output queue will be used to get the encoded data from the output defined above
         q = device.getOutputQueue(name="h265", maxSize=30, blocking=True)
 
         # The .h265 file is a raw stream file (not playable yet)
         with open('video.h265', 'wb') as videoFile:
-            print("Press Ctrl+C to stop encoding...")
+            print(f"Recording for {duration_seconds} seconds. Press Ctrl+C to stop encoding...")
+            start_time = time.time()
+
             try:
-                while True:
-                    h265Packet = q.get()  # Blocking call, will wait until a new data has arrived
+                while time.time() - start_time < duration_seconds:
+                    h265Packet = q.get()  # Blocking call, will wait until new data has arrived
                     h265Packet.getData().tofile(videoFile)  # Appends the packet data to the opened file
             except KeyboardInterrupt:
                 # Keyboard interrupt (Ctrl + C) detected
                 pass
 
-        print("To view the encoded data, convert the stream file (.h265) into a video file (.mp4) using ffmpeg on Linux or HandBrake on Mac")
-# input_file = 'video.h265'
-# output_file = 'output.mp4'
+        print("Recording complete. To view the encoded data, convert the stream file (.h265) into a video file (.mp4) using ffmpeg on Linux or HandBrake on Mac")
 
-# HandBrake on Mac also has a CLI
+# Call the function with the default duration (5 seconds)
+record_oak()
 
-# ffmpeg.input(input_file).output(output_file, vcodec='libx264', crf=28, acodec='aac', strict='experimental', b='192k').run()
+# Call the function with a custom duration (e.g., 10 seconds)
+# record_oak(duration_seconds=10)
+
